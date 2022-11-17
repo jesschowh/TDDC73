d009d700782c9263c34f7cfe16080b581a0cb80e
Tilda Hylander <hylandertilda@gmail.com>
2022-11-17 10:55:48 +0100

    Create layout for lab2

--- lab2/lib/main.dart
+++ lab2/lib/main.dart
@@ -18,7 +18,7 @@ class MyApp extends StatelessWidget {
         appBar: AppBar(
           title: const Text("Lab2"),
         ),
-        //body: const CardForm(),
+        body: const CardForm(),
       ),
     );
   }
@@ -32,38 +32,129 @@ class CardForm extends StatefulWidget {
 }
 
 class _CardFormState extends State<CardForm> {
+  final List<String> months = <String>[
+    for (var i = 1; i <= 12; i++) i.toString().padLeft(2, '0')
+  ];
+  final List<String> days = <String>[
+    for (var i = 1; i <= 31; i++) i.toString().padLeft(2, '0')
+  ];
+
   @override
   Widget build(BuildContext context) {
     return Center(
       child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            Image.asset('images/1.jpg'),
-            const Text("Card Number"),
-            Padding(
-              padding: const EdgeInsets.symmetric(horizontal: 8, vertical: 16),
-              child: TextField(
-                decoration: const InputDecoration(
-                  border: OutlineInputBorder(),
-                  hintText: 'Enter a search term',
-                ),
-                keyboardType: TextInputType.number,
-                inputFormatters: <TextInputFormatter>[
-                  FilteringTextInputFormatter.digitsOnly,
-                ],
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: <Widget>[
+          Image.asset('images/1.jpeg'),
+          Padding(
+            padding: const EdgeInsets.symmetric(horizontal: 16, vertical: 16),
+            child: TextField(
+              decoration: const InputDecoration(
+                labelText: "Card Number",
+                floatingLabelBehavior: FloatingLabelBehavior.always,
+                border: OutlineInputBorder(),
+              ),
+              keyboardType: TextInputType.number,
+              inputFormatters: <TextInputFormatter>[
+                FilteringTextInputFormatter.digitsOnly,
+              ],
+            ),
+          ),
+          const Padding(
+            padding: EdgeInsets.symmetric(horizontal: 16, vertical: 16),
+            child: TextField(
+              decoration: InputDecoration(
+                labelText: "Card Holders",
+                floatingLabelBehavior: FloatingLabelBehavior.always,
+                border: OutlineInputBorder(),
               ),
             ),
-            const Text("Card Holders"),
-            const Padding(
-              padding: EdgeInsets.symmetric(horizontal: 8, vertical: 16),
-              child: TextField(
-                decoration: InputDecoration(
-                  border: OutlineInputBorder(),
-                  hintText: 'Enter a search term',
+          ),
+          Row(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: <Widget>[
+              SizedBox(
+                width: 200,
+                child: InputDecorator(
+                  decoration: const InputDecoration(
+                    labelText: "Expiration Date",
+                    border: InputBorder.none,
+                  ),
+                  child: Row(
+                    children: <Widget>[
+                      Dropdown(
+                        drowDownList: months,
+                        textHint: "Month",
+                      ),
+                      const SizedBox(
+                        width: 25,
+                      ),
+                      Dropdown(
+                        drowDownList: days,
+                        textHint: "Days",
+                      ),
+                    ],
+                  ),
                 ),
               ),
-            ),
-          ]),
+              const SizedBox(
+                width: 25,
+              ),
+              const SizedBox(
+                width: 120,
+                child: TextField(
+                  decoration: InputDecoration(
+                    labelText: "CVV",
+                    floatingLabelBehavior: FloatingLabelBehavior.always,
+                    border: OutlineInputBorder(),
+                  ),
+                ),
+              ),
+            ],
+          ),
+        ],
+      ),
+    );
+  }
+}
+
+class Dropdown extends StatefulWidget {
+  Dropdown({super.key, required this.drowDownList, required this.textHint});
+
+  final List<String> drowDownList;
+  final String textHint;
+  String? selectedValue;
+
+  @override
+  State<StatefulWidget> createState() => _DropDownState();
+}
+
+class _DropDownState extends State<Dropdown> {
+  @override
+  Widget build(BuildContext context) {
+    return DropdownButton<String>(
+      hint: Text(widget.textHint),
+      icon: const Icon(Icons.keyboard_arrow_down),
+      elevation: 16,
+      style: const TextStyle(color: Colors.deepPurple),
+      value: widget.selectedValue,
+      underline: Container(
+        height: 2,
+        color: Colors.deepPurpleAccent,
+      ),
+      onChanged: (String? value) {
+        // This is called when the user selects an item.
+        setState(() {
+          widget.selectedValue = value!;
+        });
+      },
+      items: widget.drowDownList.map<DropdownMenuItem<String>>((String value) {
+        return DropdownMenuItem<String>(
+          value: value,
+          child: Text(value),
+        );
+      }).toList(),
+      menuMaxHeight: 400,
     );
   }
 }
