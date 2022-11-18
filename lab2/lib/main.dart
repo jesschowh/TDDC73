62bcfe470bc078b7e3d8dfdc2a2b86f030931f26
Tilda Hylander <hylandertilda@gmail.com>
2022-11-18 11:46:15 +0100

    Continued work on lab2

--- lab2/lib/main.dart
+++ lab2/lib/main.dart
@@ -39,88 +39,228 @@ class _CardFormState extends State<CardForm> {
     for (var i = 1; i <= 31; i++) i.toString().padLeft(2, '0')
   ];
 
+  var cardHolder;
+  String cardNumber = "heeeej";
+  var cardImg;
+  var cardMonth;
+  var cardDay;
+  var carCVV;
+  var cardType;
+
   @override
   Widget build(BuildContext context) {
-    return Center(
-      child: Column(
-        mainAxisAlignment: MainAxisAlignment.center,
-        children: <Widget>[
-          Image.asset('images/1.jpeg'),
-          Padding(
-            padding: const EdgeInsets.symmetric(horizontal: 16, vertical: 16),
-            child: TextField(
-              decoration: const InputDecoration(
-                labelText: "Card Number",
-                floatingLabelBehavior: FloatingLabelBehavior.always,
-                border: OutlineInputBorder(),
-              ),
-              keyboardType: TextInputType.number,
-              inputFormatters: <TextInputFormatter>[
-                FilteringTextInputFormatter.digitsOnly,
-              ],
-            ),
-          ),
-          const Padding(
-            padding: EdgeInsets.symmetric(horizontal: 16, vertical: 16),
-            child: TextField(
-              decoration: InputDecoration(
-                labelText: "Card Holders",
-                floatingLabelBehavior: FloatingLabelBehavior.always,
-                border: OutlineInputBorder(),
+    return ListView(
+      padding: const EdgeInsets.symmetric(vertical: 40),
+      children: <Widget>[
+        Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: <Widget>[
+            Padding(
+              padding: const EdgeInsets.all(10.0),
+              child: Card(
+                number: cardNumber,
               ),
             ),
-          ),
-          Row(
-            mainAxisAlignment: MainAxisAlignment.center,
-            children: <Widget>[
-              SizedBox(
-                width: 200,
-                child: InputDecorator(
-                  decoration: const InputDecoration(
-                    labelText: "Expiration Date",
-                    border: InputBorder.none,
+            Padding(
+              padding: const EdgeInsets.symmetric(horizontal: 16, vertical: 16),
+              child: TextField(
+                decoration: const InputDecoration(
+                  labelText: "Card Number",
+                  floatingLabelBehavior: FloatingLabelBehavior.always,
+                  border: OutlineInputBorder(),
+                ),
+                keyboardType: TextInputType.number,
+                inputFormatters: <TextInputFormatter>[
+                  FilteringTextInputFormatter.digitsOnly,
+                ],
+                onChanged: ((value) {
+                  setState(() {
+                    cardNumber = value;
+                  });
+                }),
+              ),
+            ),
+            const Padding(
+              padding: EdgeInsets.symmetric(horizontal: 16, vertical: 16),
+              child: TextField(
+                decoration: InputDecoration(
+                  labelText: "Card Holders",
+                  floatingLabelBehavior: FloatingLabelBehavior.always,
+                  border: OutlineInputBorder(),
+                ),
+              ),
+            ),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: <Widget>[
+                SizedBox(
+                  width: 200,
+                  child: InputDecorator(
+                    decoration: const InputDecoration(
+                      labelText: "Expiration Date",
+                      border: InputBorder.none,
+                    ),
+                    child: Row(
+                      children: <Widget>[
+                        Dropdown(
+                          drowDownList: months,
+                          textHint: "Month",
+                        ),
+                        const SizedBox(
+                          width: 25,
+                        ),
+                        Dropdown(
+                          drowDownList: days,
+                          textHint: "Days",
+                        ),
+                      ],
+                    ),
                   ),
-                  child: Row(
-                    children: <Widget>[
-                      Dropdown(
-                        drowDownList: months,
-                        textHint: "Month",
-                      ),
-                      const SizedBox(
-                        width: 25,
-                      ),
-                      Dropdown(
-                        drowDownList: days,
-                        textHint: "Days",
-                      ),
+                ),
+                const SizedBox(
+                  width: 25,
+                ),
+                SizedBox(
+                  width: 120,
+                  child: TextField(
+                    decoration: const InputDecoration(
+                      labelText: "CVV",
+                      floatingLabelBehavior: FloatingLabelBehavior.always,
+                      border: OutlineInputBorder(),
+                    ),
+                    keyboardType: TextInputType.number,
+                    inputFormatters: <TextInputFormatter>[
+                      FilteringTextInputFormatter.digitsOnly,
                     ],
                   ),
                 ),
-              ),
-              const SizedBox(
-                width: 25,
-              ),
-              const SizedBox(
-                width: 120,
-                child: TextField(
-                  decoration: InputDecoration(
-                    labelText: "CVV",
-                    floatingLabelBehavior: FloatingLabelBehavior.always,
-                    border: OutlineInputBorder(),
-                  ),
-                ),
-              ),
-            ],
-          ),
-        ],
-      ),
+              ],
+            ),
+          ],
+        ),
+      ],
     );
   }
 }
 
+Text getCardText() {
+  return const Text(
+    'Hello World',
+    style: TextStyle(color: Colors.deepPurpleAccent),
+  );
+}
+
+class Card extends StatefulWidget {
+  Card({super.key, required this.number});
+  String number;
+  var imgRatio = 675 / 435;
+  @override
+  State<StatefulWidget> createState() => _CardState();
+}
+
+class _CardState extends State<Card> {
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      decoration: BoxDecoration(
+          borderRadius: BorderRadius.circular(8.0),
+          image: const DecorationImage(
+            image: AssetImage('images/19.jpeg'),
+            fit: BoxFit.cover,
+          )),
+      child: SizedBox(
+        width: 250 * widget.imgRatio,
+        height: 250,
+        child: Padding(
+          padding: const EdgeInsets.symmetric(horizontal: 20.0),
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+            children: <Widget>[
+              //chip and card type
+              Row(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                children: [
+                  Image.asset('images/chip.png'),
+                  SizedBox(
+                    height: 50,
+                    width: 50,
+                    child: Image.asset('images/mastercard.png'),
+                  ),
+                ],
+              ),
+              // card number
+              Text(widget.number),
+              // card holder and expiry date
+              Row(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                children: [
+                  Column(
+                    children: <Widget>[
+                      const Text("Card Holder"),
+                      Text(widget.number),
+                    ],
+                  ),
+                  Column(
+                    children: [
+                      const Text("Expires"),
+                      Text(widget.number),
+                    ],
+                  ),
+                ],
+              )
+            ],
+          ),
+        ),
+      ),
+    );
+
+    // Stack(
+    //   children: <Widget>[
+    //     Image.asset('images/19.jpeg'),
+    //     Column(
+    //       mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+    //       children: <Widget>[
+    //         // chip and card type
+    //         Row(
+    //           children: [
+    //             Image.asset('images/chip.png'),
+    //             SizedBox(
+    //               height: 50,
+    //               width: 50,
+    //               child: Image.asset('images/mastercard.png'),
+    //             ),
+    //           ],
+    //         ),
+    //         // card number
+    //         Text(widget.number),
+    //         // card holder and expiry date
+    //         Row(
+    //           children: [
+    //             Column(
+    //               children: <Widget>[
+    //                 const Text("Card Holder"),
+    //                 Text(widget.number),
+    //               ],
+    //             ),
+    //             Column(
+    //               children: [
+    //                 const Text("Expires"),
+    //                 Text(widget.number),
+    //               ],
+    //             ),
+    //           ],
+    //         ),
+    //       ],
+    //     ),
+    //   ],
+    // );
+  }
+}
+
+// ignore: must_be_immutable
 class Dropdown extends StatefulWidget {
   Dropdown({super.key, required this.drowDownList, required this.textHint});
-
   final List<String> drowDownList;
   final String textHint;
   String? selectedValue;
